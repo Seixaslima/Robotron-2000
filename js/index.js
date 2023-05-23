b7992bb84dff0c102f78f787351879893519d169
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-05-23 15:48:31 -0300

    exercicios unidade 5

--- js/index.js
+++ js/index.js
@@ -1,8 +1,43 @@
+const pecas = {
+  bracos: {
+    forca: 29,
+    poder: 35,
+    energia: -21,
+    velocidade: -5
+  },
+
+  blindagem: {
+    forca: 41,
+    poder: 20,
+    energia: 0,
+    velocidade: -20
+  },
+  nucleos: {
+    forca: 0,
+    poder: 7,
+    energia: 48,
+    velocidade: -24
+  },
+  pernas: {
+    forca: 27,
+    poder: 21,
+    energia: -32,
+    velocidade: 42
+  },
+  foguetes: {
+    forca: 0,
+    poder: 28,
+    energia: 0,
+    velocidade: -2
+  }
+};
 const controle = document.querySelectorAll('[data-controle]');
+const estatistica = document.querySelectorAll('[data-estatistica]');
 
 controle.forEach(elemento => {
   elemento.addEventListener('click', evento => {
     manipulaEvento(evento.target.dataset.controle, evento.target.parentNode);
+    atualizaEstatistica(evento.target.dataset.peca);
   });
 });
 
@@ -15,4 +50,12 @@ function manipulaEvento(operacao, controle) {
   if (operacao === '-') {
     peca.value = parseInt(peca.value) - 1;
   }
+}
+
+function atualizaEstatistica(peca) {
+  console.log(pecas[peca]);
+  estatistica.forEach(evento => {
+    evento.innerText =
+      parseInt(evento.innerText) + pecas[peca][evento.dataset.estatistica];
+  });
 }
